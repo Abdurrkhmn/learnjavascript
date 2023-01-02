3850e5b05ddd7851af424d1732f94bb274553ce2
Abdurrkhmn <Abdurrahmansy25@gmail.com>
2023-01-02 23:39:34 +0300

    update

---
+++ Kuis Coding/Variabel-Datatype.js
@@ -0,0 +1,20 @@
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai nama depan Anda.
+ *  - lastName: bertipe data string, dengan nilai nama belakang Anda.
+ *  - age: bertipe data number, dengan nilai umur Anda.
+ *  - isMarried: bertipe data boolean, dengan bebas Anda tentukan.
+ */
+
+let firstName = "Abdurrahman";
+console.log(firstName)
+let lastName = "No";
+console.log(lastName)
+let age = 25;
+console.log(age)
+let isMarried = true;
+console.log(isMarried)
+module.exports = {
+    firstName, lastName, age, isMarried
+}
