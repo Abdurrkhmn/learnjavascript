193d3f841f85098e8c80ea9e187b5b6694f48eda
Abdurrkhmn <Abdurrahmansy25@gmail.com>
2023-01-03 15:49:44 +0300

    update

---
+++ Struktur Data/Objects/assignmentoperator.js
@@ -0,0 +1,50 @@
+/**
+ * Yang perlu diperhatikan adalah mengubah nilai berbeda dengan menginisialisasi ulang nilai. 
+ * Ketika kita mengubah object menggunakan assignment operator dan property/key-nya sudah ada, 
+ * maka nilai di dalamnya akan tergantikan dengan nilai yang baru. dan sebaliknya
+ */
+const spaceship = {
+    name: "Millenium Falcon",
+    manufacturer: "Corellian Engineering Corporation",
+    maxSpeed: 1200,
+    color: "Light gray"
+};
+
+spaceship.color = "Glossy red";
+spaceship["maxSpeed"] = 1300;
+spaceship.class = "Light freighter";
+
+console.log(spaceship);
+
+/* output
+{
+  name: 'Millenium Falcon',
+  manufacturer: 'Corellian Engineering Corporation',
+  maxSpeed: 1300,
+  color: 'Glossy red',
+  class: 'Light freighter'
+}
+*/
+
+
+/**
+ Kita juga dapat menghapus property pada object menggunakan keyword delete seperti berikut:
+Example
+ const spaceship = {
+    name: "Millenium Falcon",
+    manufacturer: "Corellian Engineering Corporation",
+    maxSpeed: 1200,
+    color: "Light gray"
+};
+
+spaceship.color = "Glossy red";
+spaceship["maxSpeed"] = 1300;
+
+delete spaceship.manufacturer;
+
+console.log(spaceship);
+
+output
+{ name: 'Millenium Falcon', maxSpeed: 1300, color: 'Glossy red' }
+ */
+ 
